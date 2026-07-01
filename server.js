de09dff5429c89d8ca15111dc93ddc7e3bb01d4c
MaanyataAul004 <maanyata.aul1302@gmail.com>
2026-07-02 05:09:28 +0530

    smthing

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ const PORT = process.env.PORT || 8080;
 //run listen
 app.listen(PORT, () => {
   console.log(
-    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
-      .white
-  );
+    `Server Running on Port ${PORT}`
+    );
+    
 });
