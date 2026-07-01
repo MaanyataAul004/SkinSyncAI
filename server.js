480d7cf34d0e07446984eb810d940d3e23d6dd0e
MaanyataAul004 <maanyata.aul1302@gmail.com>
2026-07-02 03:46:24 +0530

    deployment changes

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
-import authRoutes from "./routes/authRoute.js";
 import aiRoutes from "./routes/aiRoute.js";
 import cors from "cors";
 
@@ -22,7 +21,6 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 //routes
-app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/ai", aiRoutes);
 
 //rest api
